f21f0a6b0b8a1def5581b4cfa76a6edf86959d64
Bram Waasdorp <bram@waasdorpsoekhan.nl>
2017-07-20 23:13:09 +0200

    Create wsaallotment.php

---
+++ wsaallotment.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: wsa allotment
+Plugin URI:  http://www.waasdorpsoekhan.nl/wsaallotment
+Description: Plugin for management allotment entities
+Version:     0.1
+Author:      Bram Waasdorp
+Author URI:  https://www.waasdorpsoekhan.nl/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wsaallotment
+Domain Path: /languages
+*/
