540c875aa37c2791e76abbdbbac686ef344a3866
Bram Waasdorp <bram@waasdorpsoekhan.nl>
2017-07-20 23:15:51 +0200

    Update wsaallotment.php

--- wsaallotment.php
+++ wsaallotment.php
@@ -10,4 +10,17 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: wsaallotment
 Domain Path: /languages
+
+wsa allotment is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+wsa allotment is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with wsa allotment. If not, see https://www.gnu.org/licenses/gpl-2.0.html
 */
