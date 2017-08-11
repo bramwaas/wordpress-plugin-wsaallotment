7d45485b2890feaea2e518dab6c4a624ff1aadb9
Bram Waasdorp <bram@waasdorpsoekhan.nl>
2017-08-11 07:15:15 +0200

    section table toegevoegd

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,7 @@
  * @copyright  Copyright (c)  2017, Bram Waasdorp
  * @link       https://github.com/bramwaas/wordpress-plugin-wsaallotment
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 20170811    section table toegevoegd
  */
 // if uninstall.php is not called by WordPress, die
 if (!defined('WP_UNINSTALL_PLUGIN')) {
@@ -20,10 +21,13 @@ wsaallotment_memberadministration_role_remove();
 global $wpdb;
 $table_name_gardener  = $wpdb->prefix . "gardener";
 $table_name_allotment = $wpdb->prefix . "allotment";
+$table_name_section = $wpdb->prefix . "section";
 
 $wpdb->query("DROP TABLE IF EXISTS $table_name_gardener");
 $wpdb->query("DROP TABLE IF EXISTS $table_name_allotment");
+$wpdb->query("DROP TABLE IF EXISTS $table_name_section");
 //remove options added by this plugin 
 $option_name = 'wsaallotment_db_version';
- 
+delete_option($option_name);
+$option_name = 'wsaallotment_version';
 delete_option($option_name);
